cd7794c82b699dd235e6f21121674151c24a9057
werbenhu <werbenhu@aliyun.com>
2024-12-24 18:51:17 +0800

    Add some benchmark test.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/werbenhu/skiplist
 
 go 1.22.1
+
+require github.com/sean-public/fast-skiplist v0.0.0-20200308194023-d7f7945b944e
---
+++ skiplist_bench_test.go
@@ -0,0 +1,54 @@
+package skiplist
+
+import (
+	"math/rand/v2"
+	"strconv"
+	"testing"
+
+	fastskiplist "github.com/sean-public/fast-skiplist"
+)
+
+// Insert keys randomly
+func BenchmarkSkipListInsertRandom(b *testing.B) {
+	list := NewSkiplist[float64, int]()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		list.Insert(rand.Float64(), i)
+	}
+}
+
+// Insert keys in sorted order
+func BenchmarkSkipListInsertSorted(b *testing.B) {
+	list := NewSkiplist[float64, int]()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		list.Insert(float64(i), i)
+	}
+}
+
+// Insert keys randomly
+func BenchmarkFastSkipListInsertRandom(b *testing.B) {
+	fast := fastskiplist.New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fast.Set(rand.Float64(), i)
+	}
+}
+
+// Insert keys in sorted order
+func BenchmarkFastSkipListInsertSorted(b *testing.B) {
+	fast := fastskiplist.New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fast.Set(float64(i), i)
+	}
+}
+
+func BenchmarkMapSet(b *testing.B) {
+	n := make(map[string]float64, 0)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		score := rand.Float64() * 1000
+		n[strconv.Itoa(i)] = score
+	}
+}
